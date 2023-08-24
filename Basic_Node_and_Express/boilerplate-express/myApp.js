66430288ba1fda5e1f93c2ded67ddd9da3806978
Sam-Fishwick <sam.fishwick@hotmail.com>
2023-08-25 00:45:50 +0100

    Updating myApp

--- Basic_Node_and_Express/boilerplate-express/myApp.js
+++ Basic_Node_and_Express/boilerplate-express/myApp.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 
 app.listen(3000);
 
@@ -8,6 +9,8 @@ app.use((req, res, next) => {
     console.log(req.method + " " + req.path + " - " + req.ip);
     next();
 })
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(__dirname+'/public'));
 
@@ -27,7 +30,24 @@ app.get('/now', (req, res, next) => {
     req.time = new Date().toString();
     next();
 }, (req, res) => {
-    res.json({"time" : req.time});
+    res.json({"time": req.time});
+});
+
+app.get('/:word/echo', (req, res) => {
+    res.json({"echo": req.params.word});
+});
+
+app.route('/name').get((req, res) => {
+    if(typeof req.query.first != "undefined" 
+        && typeof req.query.last != "undefined"){
+        let string = req.query.first+" "+req.query.last;
+        res.json({"name": string});
+    }else{
+        res.send("Give name in format 'URL/name?first=FIRST&last=LAST'");
+    };
+}).post((req, res) => {
+    let string = req.body.first+" "+req.body.last;
+    res.json({"name": string});
 });
 
 module.exports = app;
