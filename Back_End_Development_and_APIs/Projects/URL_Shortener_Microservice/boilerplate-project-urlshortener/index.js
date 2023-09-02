c1257f91bb3fd1e9e6c534b524f1558b5045cd43
Sam-Fishwick <sam.fishwick@hotmail.com>
2023-09-02 17:23:03 +0100

    Updating index.js to show code that passed tests

--- Back_End_Development_and_APIs/Projects/URL_Shortener_Microservice/boilerplate-project-urlshortener/index.js
+++ Back_End_Development_and_APIs/Projects/URL_Shortener_Microservice/boilerplate-project-urlshortener/index.js
@@ -2,11 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient } = require('mongodb');
+const dns = require('dns');
+const urlparser = require('url');
+
+const client = new MongoClient(process.env.MY_URI);
+const db = client.db('db_short_url');
+const collection = db.collection('collection_short_url');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,6 +26,33 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url;
+  const dnslookup = dns.lookup(urlparser.parse(url).hostname,
+    async (error, data) => {
+      if (!data){
+        res.json({error: "Invalid URL"});
+      }else{
+        const urlCount = await collection.countDocuments({});
+        const urlDoc = {
+          url,
+          short_url: urlCount
+        }
+        
+        const result = await collection.insertOne(urlDoc);
+        console.log(result);
+        res.json({original_url: url, short_url: urlCount});
+
+      }
+    });
+});
+
+app.get('/api/shorturl/:short_url', async (req, res) => {
+  const shorturl = req.params.short_url;
+  const urlDoc = await collection.findOne({short_url: +shorturl});
+  res.redirect(urlDoc.url);
 });
 
 app.listen(port, function() {
